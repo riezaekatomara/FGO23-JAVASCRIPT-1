9bd33c8af6fc68656c6c72cfa39e85e62e72ad16
riezaekatomara <130181217+riezaekatomara@users.noreply.github.com>
2025-02-13 16:50:48 +0700

    Mini Task Day 3 SOAL 1

---
+++ soal1-day3.js
@@ -0,0 +1,12 @@
+asean = ["Indonesia", "Thailand", "Singapura"];
+
+let person = {
+    firstName: "Rieza",
+    middleName: "Eka",
+    lastName: "Tomara",
+    age: 27,
+    provinsi: "Sumatera Selatan"
+};
+
+console.log(person.middleName);
+console.log(asean[0]);
